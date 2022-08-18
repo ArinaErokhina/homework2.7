48e69c07c2cb356602e0d6474a8234f6bdb5a2e5
Lu <lokka.rei@yandex.ru>
2022-08-18 23:50:45 +0300

    Initial commit

---
+++ src/test/java/com/homework27/homework27/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework27.homework27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
